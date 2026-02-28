2adcd9bde0545f095d70fd3c44db8e05a36796ab
Alex Marvick <amarvick94@gmail.com>
2026-02-27 21:25:53 -0500

    Setting up openAI API info

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,10 @@
 import os
 
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-insecure-change-me") #TODO - change this later. MUST be changed before production!!!!!!
+SECRET_KEY = os.getenv("SECRET_KEY", "") #TODO - change this later. MUST be changed before production!!!!!!
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "300"))
+
+# AI Interview configuration
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4.1-mini")
